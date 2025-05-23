8364a520245d2ee341f3afe57466c2416222c06c
alistkov <antonlistkov@gmail.com>
2025-05-23 20:19:35 +0200

    setup build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    id("java")
+    application
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -8,6 +12,7 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
