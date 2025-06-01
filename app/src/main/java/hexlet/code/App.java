247181e6fb6ed4aa0c029b27234bf37463e7f252
alistkov <antonlistkov@gmail.com>
2025-06-02 01:20:27 +0200

    update info message

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,44 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+@CommandLine.Command(
+    name = "gendiff",
+    description = "Compares two configuration files and shows a difference.",
+    version = "0.0.2",
+    mixinStandardHelpOptions = true
+)
 public class App {
+    @CommandLine.Parameters(
+        paramLabel = "filePath1",
+        description = "path to first file")
+    private String filePath1;
+
+    @CommandLine.Parameters(
+        paramLabel = "filePath2",
+        description = "path to second file")
+    private String filePath2;
+
+    @CommandLine.Option(
+        names = {"-h", "--help"},
+        usageHelp = true,
+        description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @CommandLine.Option(
+        names = { "-V", "--version" },
+        versionHelp = true,
+        description = "Print version information and exit.")
+    boolean versionRequested;
+
+    @CommandLine.Option(
+        names = { "-f", "--format" },
+        defaultValue = "stylish",
+        paramLabel = "format",
+        description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
+
     public static void main(String[] args) {
-        new CommandLine(new Differ()).execute(args);
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
